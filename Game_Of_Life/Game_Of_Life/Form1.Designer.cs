01612647b448318e4c92fcaad69ae97e90ea593e
issame <issame.kastite@gmail.com>
2014-06-09 18:09:55 +0100

    First commit
    Conway's Game of Life

---
+++ Game_Of_Life/Game_Of_Life/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Game_Of_Life
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDraw.Location = new System.Drawing.Point(683, 12);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(77, 23);
+            this.btnDraw.TabIndex = 0;
+            this.btnDraw.Text = "Draw Grid";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // txtX
+            // 
+            this.txtX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtX.Location = new System.Drawing.Point(710, 42);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(49, 20);
+            this.txtX.TabIndex = 1;
+            this.txtX.Text = "10";
+            this.txtX.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(685, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "X :";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(685, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Y :";
+            // 
+            // txtY
+            // 
+            this.txtY.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtY.Location = new System.Drawing.Point(710, 68);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(49, 20);
+            this.txtY.TabIndex = 3;
+            this.txtY.Text = "10";
+            this.txtY.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(683, 400);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Stop life";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(683, 371);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Start life";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(682, 94);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(77, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnDraw;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 435);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.btnDraw);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Game_Of_Life/Game_Of_Life/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Game_Of_Life
+{
+    public partial class Form1 : Form
+    {
+        int SIZEX = 10;
+        int SIZEY = 10;
+        const int CELLSIZE = 10;
+        Cell[,] Life;
+        bool[,] nextLife;
+
+        System.Drawing.Graphics graphicsObj;
+        Pen myPen = new Pen(System.Drawing.Color.Black, 1);
+
+        #region Form
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        #endregion
+
+        #region Buttons
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            graphicsObj = this.CreateGraphics();
+            try
+            {
+                SIZEX = int.Parse(txtX.Text);
+                SIZEY = int.Parse(txtY.Text);
+                btnDraw.Enabled = false;
+                txtX.Enabled = false;
+                txtY.Enabled = false;
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Données non valides!");
+            }
+
+            Life = new Cell[SIZEX, SIZEY];
+            nextLife = new bool[SIZEX, SIZEY];
+
+            for (int x = 0; x < SIZEX; x++)
+            {
+                for (int y = 0; y < SIZEY; y++)
+                {
+                    Life[x, y] = new Cell();
+                    Life[x, y].cell = new Rectangle((x * CELLSIZE), (y * CELLSIZE), CELLSIZE, CELLSIZE);
+                    graphicsObj.FillRectangle(new SolidBrush(Color.White), Life[x, y].cell);
+                    graphicsObj.DrawRectangle(myPen, Life[x, y].cell);
+                    Life[x, y].alive = false;
+                }
+            }
+
+            button2.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button1_Click(null, null);
+            btnDraw.Enabled = true;
+            button2.Enabled = false;
+            txtY.Enabled = true;
+            txtX.Enabled = true;
+            for (int x = 0; x < SIZEX; x++)
+            {
+                for (int y = 0; y < SIZEY; y++)
+                {
+                    graphicsObj.FillRectangle(new SolidBrush(Color.White), Life[x, y].cell);
+                    graphicsObj.DrawRectangle(myPen, Life[x, y].cell);
+                    Life[x, y].alive = false;
+                }
+            }
+            Life = null;
+            this.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            button1.Enabled = true;
+            timer2.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            timer2.Stop();
+        }
+        #endregion
+
+        #region Timer
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            color();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            run();
+        }
+        #endregion
+
+        #region Logic
+        private void run()
+        {
+            for (int i = 0; i < SIZEX; i++)
+            {
+                for (int j = 0; j < SIZEY; j++)
+                {
+						//int count = checkvecinos(i, j);
+                        //int vecinos = entorno(i, j);
+                        int count = entorno(i, j);
+						bool result = false;
+
+                        if (Life[i, j].alive)
+                        {
+                            if ((count == 2 || count == 3))
+                                result = true;
+                            else result = false;
+                        }
+
+                        if (!Life[i, j].alive)
+                        {
+                            if (count == 3)
+                                result = true;
+                            else result = false;
+                        }
+
+                        nextLife[i, j] = result;
+                }
+            }
+
+            for (int i = 0; i < SIZEX; i++)
+            {
+                for (int j = 0; j < SIZEY; j++)
+                {
+                    Life[i, j].alive = nextLife[i,j];
+
+                    if (Life[i, j].alive)
+                    {
+                        graphicsObj.FillRectangle(new SolidBrush(Color.Blue), Life[i, j].cell);
+                        graphicsObj.DrawRectangle(myPen, Life[i, j].cell);
+                    }
+                    else
+                    {
+                        graphicsObj.FillRectangle(new SolidBrush(Color.White), Life[i, j].cell);
+                        graphicsObj.DrawRectangle(myPen, Life[i, j].cell);
+                    }
+                }
+            }
+        }
+
+        private int entorno(int x, int y)
+        {
+            int c = 0;
+
+            int si = (x - 1 < 0) ? 0 : x - 1;
+            int sj = (y - 1 < 0) ? 0 : y - 1;
+            int fi = (x + 2 > SIZEX) ? SIZEX : x + 2;
+            int fj = (y + 2 > SIZEY) ? SIZEY : y + 2;
+
+            for (int i = si; i < fi; i++)
+            {
+                for (int j = sj; j < fj; j++)
+                {
+                    if (i != x || j != y)
+                        if (Life[i, j].alive)
+                        {
+                            c++;
+                        }
+                }
+
+            }
+
+            return c;
+        }
+        #endregion
+
+        #region Cell paint
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Start();
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void color()
+        {
+            Point point = this.PointToClient(MousePosition);
+
+            int x = point.X / CELLSIZE;
+            int y = point.Y / CELLSIZE;
+
+            try
+            {
+                if (!Life[x, y].alive)
+                {
+                    graphicsObj.FillRectangle(new SolidBrush(Color.Blue), Life[x, y].cell);
+                    graphicsObj.DrawRectangle(myPen, Life[x, y].cell);
+                    Life[x, y].alive = true;
+                }
+                else
+                {
+                    graphicsObj.FillRectangle(new SolidBrush(Color.White), Life[x, y].cell);
+                    graphicsObj.DrawRectangle(myPen, Life[x, y].cell);
+                    Life[x, y].alive = false;
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+        #endregion
+
+        #region Class Cell
+        class Cell
+        {
+            public Rectangle cell { get; set; }
+            public bool alive { get; set; }
+        }
+        #endregion
+
+
+    }
+}
